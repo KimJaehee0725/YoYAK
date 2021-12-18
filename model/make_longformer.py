c4edb451d617109a722f670089d8d45b674dde9d
Lainshower <kaoara@naver.com>
2021-12-18 16:30:47 +0900

    modify make_longformer.py and add train.py

--- model/make_longformer.py
+++ model/make_longformer.py
@@ -1,8 +1,10 @@
+import random
+import copy
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
 import torch
 import torch.nn as nn
-
-from dataclasses import dataclass, field
-from typing import Dict, List, Optional, Tuple
 
 from transformers import BartForConditionalGeneration, BartConfig
 from transformers import PreTrainedTokenizerFast
@@ -10,16 +12,13 @@ from transformers import PreTrainedTokenizerFast
 from transformers.models.bart.modeling_bart import BartLearnedPositionalEmbedding
 from transformers.models.longformer.modeling_longformer import LongformerSelfAttention
 
-
-# Kobart의 attention layer를 대체.
-
+# Kobart의 attention layer를 대체
 class LongformerSelfAttentionForBart(nn.Module):
-    def __init__(self, config, layer_id):
+    def __init__(self, config : dict , layer_id : int):
         super().__init__()
         self.embed_dim = config.d_model
         self.longformer_self_attn = LongformerSelfAttention(config, layer_id=layer_id)
         self.output = nn.Linear(self.embed_dim, self.embed_dim)
-
 
     # kobart의 기존 layer와 동일한 형태의 입력을 받고, 동일한 형태의 출력을 할 수 있도록 해줘야함.
     def forward(self,
@@ -59,14 +58,9 @@ class LongformerSelfAttentionForBart(nn.Module):
 class LongformerBartForConditionalGeneration(BartForConditionalGeneration):
     def __init__(self, config):
         super().__init__(config)
-        print(config.max_encoder_position_embeddings)
-        print(config.d_model)
-        print(config.pad_token_id)
-        
         if config.attention_mode == 'n2':
             pass  # do nothing, use BertSelfAttention instead
         else:
-
             self.model.encoder.embed_positions = BartLearnedPositionalEmbedding(
                 config.max_encoder_position_embeddings, 
                 config.d_model, 
@@ -76,12 +70,11 @@ class LongformerBartForConditionalGeneration(BartForConditionalGeneration):
                 config.max_decoder_position_embeddings, 
                 config.d_model, 
                 config.pad_token_id)
-
+            
             for i, layer in enumerate(self.model.encoder.layers):
                 layer.self_attn = LongformerSelfAttentionForBart(config, layer_id=i)
 
 #longformer bart모델의 config 생성 class
-
 class LongformerBartConfig(BartConfig):
     def __init__(self, attention_window: List[int] = [512], attention_dilation: List[int] = [1],
                  autoregressive: bool = False, attention_mode: str = 'sliding_chunks',
@@ -98,69 +91,74 @@ class LongformerBartConfig(BartConfig):
             attention_mode: 'n2' for regular n^2 self-attention, 'tvm' for TVM implemenation of Longformer
                 selfattention, 'sliding_chunks' for another implementation of Longformer selfattention
         """
+
         super().__init__(**kwargs)
-
-        self.attention_window = attention_window*self.num_hidden_layers # longformer config에 추가
-        self.attention_dilation = attention_dilation*self.num_hidden_layers # longformer config에 추가
-        self.autoregressive = autoregressive # longformer config에 추가 >  False
-        self.attention_mode = attention_mode # longformer config에 추가 > 'sliding_chunks'
-        self.gradient_checkpointing = gradient_checkpointing # longformer config에 추가 > False
-
-        self.max_seq_len = max_seq_len # max_seq_len 추가
-        self.attention_probs_dropout_prob = self.attention_dropout # attention_dropout > attention_probs_dropout_prob로 hparams 명 변경
-        self.architectures = ['LongformerBartForConditionalGeneration', ]
-
-        self.max_encoder_position_embeddings = max_pos # enconder positional-embedding 확장
-        self.max_decoder_position_embeddings = self.max_position_embeddings # decoder positional-embedding은 그대로
-        del self.max_position_embeddings # 기존에 encoder, decoder 둘 다 동시에 적용되었던 max_positional_embeddings는 삭제 
-        
+        self.attention_window = attention_window
+        self.attention_dilation = attention_dilation
+        self.autoregressive = autoregressive
+        self.attention_mode = attention_mode
+        self.gradient_checkpointing = gradient_checkpointing
         assert self.attention_mode in ['tvm', 'sliding_chunks', 'n2']
 
-save_path = 'longformer_kobart'
 
-max_pos = 4104
-max_seq_len = 4096
+if __name__ == '__main__':
+	max_pos = 4104
+	max_seq_len = 4096
+	attention_window = 512
+	save_path = 'longformer_kobart'
 
-tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-base-v1', model_max_length=max_pos)
-kobart_longformer = BartForConditionalGeneration.from_pretrained('gogamza/kobart-base-v1')
-kobart_longformer.config  = LongformerBartConfig.from_pretrained('gogamza/kobart-base-v1')
+	# 기존 pretrained 된 kobart tokenizer & model load
+	tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-base-v1', model_max_length=max_pos)
+	kobart_longformer = BartForConditionalGeneration.from_pretrained('gogamza/kobart-base-v1')
+	config  = LongformerBartConfig.from_pretrained('gogamza/kobart-base-v1')
 
-# Tokenizer의 max_positional_embedding_size config 확장
-tokenizer.model_max_length = max_pos
-tokenizer.init_kwargs['model_max_length'] = max_pos
-kobart_longformer.model.encoder.embed_positions.weight.shape
-#assert current_max_pos == kobart_longformer.max_position_embeddings + 2 # +2는 bos,eos를 위한 것
+	kobart_longformer.config = config
 
-current_max_pos , embed_size = kobart_longformer.model.encoder.embed_positions.weight.shape
+	config.attention_probs_dropout_prob = config.attention_dropout
+	config.architectures = ['LongformerEncoderDecoderForConditionalGeneration', ]
 
-max_pos += 2  # NOTE: BART has positions 0,1 reserved, so embedding size is max position + 2
-assert max_pos >= current_max_pos
 
-new_encoder_pos_embed = kobart_longformer.model.encoder.embed_positions.weight.new_empty(max_pos, embed_size)
+	# Tokenizer의 max_positional_embedding_size 확장
+	# extend position embeddings
+	tokenizer.model_max_length = max_pos
+	tokenizer.init_kwargs['model_max_length'] = max_pos
+	current_max_pos, embed_size = kobart_longformer.model.encoder.embed_positions.weight.shape
+	assert current_max_pos == config.max_position_embeddings + 2
 
-# PE 대체
-k = 2
-step = 1028 - 2
-while k < max_pos - 1:
-    new_encoder_pos_embed[k:(k + step)] = kobart_longformer.model.encoder.embed_positions.weight[2:]
-    k += step
-kobart_longformer.model.encoder.embed_positions.weight.data = new_encoder_pos_embed
+	config.max_encoder_position_embeddings = max_pos
+	config.max_decoder_position_embeddings = config.max_position_embeddings
+	del config.max_position_embeddings
+	max_pos += 2  # NOTE: BART has positions 0,1 reserved, so embedding size is max position + 2
+	assert max_pos >= current_max_pos
 
-# Layer 대체
-for i, layer in enumerate(kobart_longformer.model.encoder.layers):
-    longformer_self_attn_for_bart = LongformerSelfAttentionForBart(kobart_longformer.config, layer_id=i)
+	new_encoder_pos_embed = kobart_longformer.model.encoder.embed_positions.weight.new_empty(max_pos, embed_size)
 
-    longformer_self_attn_for_bart.longformer_self_attn.query = layer.self_attn.q_proj
-    longformer_self_attn_for_bart.longformer_self_attn.key = layer.self_attn.k_proj
-    longformer_self_attn_for_bart.longformer_self_attn.value = layer.self_attn.v_proj
+	# Positional Embedding 확장
+	k = 2
+	step = 1028 - 2
+	while k < max_pos - 1:
+		new_encoder_pos_embed[k:(k + step)] = kobart_longformer.model.encoder.embed_positions.weight[2:]
+		k += step
+	kobart_longformer.model.encoder.embed_positions.weight.data = new_encoder_pos_embed
 
-    longformer_self_attn_for_bart.longformer_self_attn.query_global = copy.deepcopy(layer.self_attn.q_proj)
-    longformer_self_attn_for_bart.longformer_self_attn.key_global = copy.deepcopy(layer.self_attn.k_proj)
-    longformer_self_attn_for_bart.longformer_self_attn.value_global = copy.deepcopy(layer.self_attn.v_proj)
+	config.attention_window = [attention_window] * config.num_hidden_layers
+	config.attention_dilation = [1] * config.num_hidden_layers
 
-    longformer_self_attn_for_bart.output = layer.self_attn.out_proj
+	# Kobart Self attention > Longformer Self Attention
+	for i, layer in enumerate(kobart_longformer.model.encoder.layers):
+		longformer_self_attn_for_bart = LongformerSelfAttentionForBart(kobart_longformer.config, layer_id=i)
 
-    layer.self_attn = longformer_self_attn_for_bart
+		longformer_self_attn_for_bart.longformer_self_attn.query = layer.self_attn.q_proj
+		longformer_self_attn_for_bart.longformer_self_attn.key = layer.self_attn.k_proj
+		longformer_self_attn_for_bart.longformer_self_attn.value = layer.self_attn.v_proj
 
-kobart_longformer.save_pretrained(save_path)
-tokenizer.save_pretrained(save_path, None)
+		longformer_self_attn_for_bart.longformer_self_attn.query_global = copy.deepcopy(layer.self_attn.q_proj)
+		longformer_self_attn_for_bart.longformer_self_attn.key_global = copy.deepcopy(layer.self_attn.k_proj)
+		longformer_self_attn_for_bart.longformer_self_attn.value_global = copy.deepcopy(layer.self_attn.v_proj)
+
+		longformer_self_attn_for_bart.output = layer.self_attn.out_proj
+
+		layer.self_attn = longformer_self_attn_for_bart
+
+	kobart_longformer.save_pretrained(save_path)
+	tokenizer.save_pretrained(save_path, None)
---
+++ model/train.py
@@ -0,0 +1,184 @@
+import os
+import argparse
+import random
+import numpy as np
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+from transformers.optimization import AdamW
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+
+from make_longformer import *
+from transformers import PreTrainedTokenizerFast
+
+# Dataset이 넘겨줘야할 것들
+# inputs : encoder_inputs : encoder input_ids, attention_mask
+# inputs : decoder input_ids, attention_mask
+# label : input_ids 
+
+class LongformerSummaryModule(pl.LightningDataModule):
+    def __init__(self, train_file:str, valid_file:str, test_file:str, tokenizer_path:str, batch_size: int=8, num_workers: int=5):
+        super().__init__()
+        self.batch_size = batch_size
+        self.train_file_path = train_file
+        self.valid_file_path = valid_file
+        self.test_file_path = test_file
+        self.tokenizer = PreTrainedTokenizerFast.from_pretrained(tokenizer_path)
+        self.num_workers = num_workers
+
+    # OPTIONAL, called for every GPU/machine (assigning state is OK)
+    def setup(self, stage):
+        # split dataset
+        self.train = iterableDataset(self.train_file_path)
+        self.valid = iterableDataset(self.valid_file_path)
+        self.test = iterableDataset(self.test_file_path)
+
+    def train_dataloader(self):
+        train = DataLoader(self.train, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+        return train
+
+    def val_dataloader(self):
+        val = DataLoader(self.test, batch_size=self.batch_size,  num_workers=self.num_workers, shuffle=False)
+        return val
+
+    def test_dataloader(self):
+        test = DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
+        return test
+
+
+class LongformerKobart(pl.LightningModule):
+    def __init__(self, params):
+        super().__init__()
+        self.args = params
+        self.model = LongformerBartForConditionalGeneration.from_pretrained(args.model_path)
+        self.tokenizer = PreTrainedTokenizerFast.from_pretrained(self.args.tokenizer_path)
+        self.bos_token = '<s>'
+        self.eos_token = '</s>'
+        self.pad_token_id = 3
+        self.ignore_token_id= -100
+
+    # 어떤식으로 Input 넘겨서 받를지 확인하기
+    def forward(self, inputs):
+        encoder_inputs, decoder_inputs, labels = inputs
+
+        return self.model(input_ids=encoder_inputs['token_ids'],
+                          attention_mask=encoder_inputs['attention_mask'],
+                          decoder_input_ids=decoder_inputs['token_ids'],
+                          decoder_attention_mask=decoder_inputs['attention_mask'],
+                          labels=labels['token_ids'], return_dict=True)
+    
+    # Use this method to caculate rouge score
+    def generate(self,inputs):
+        encoder_inputs, decoder_inputs, labels = inputs
+        return self.model.generate(input_ids=encoder_inputs['input_ids'], 
+                                    attention_mask=encoder_inputs['attention_mask'],
+                                    max_length=self.args.max_output_len,
+                                    eos_token_id=self.tokenizer.eos_token_id,
+                                    num_beams=1), labels['token_ids']
+
+
+    def training_step(self, batch, batch_idx):
+        outs = self(batch)
+        loss = outs.loss
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        outs = self(batch)
+        loss = outs['loss']
+        return (loss)
+
+    def validation_epoch_end(self, outputs):
+        losses = []
+        for loss in outputs:
+            losses.append(loss)
+        self.log('val_loss', torch.stack(losses).mean(), prog_bar=True)
+    
+    # TO DO !
+    # Add rouge score in test step
+    def test_step(self, batch, batch_nb):
+        return self.validation_step(batch, batch_nb)
+
+    def test_epoch_end(self, outputs):
+        self.validation_epoch_end(outputs)
+
+    def configure_optimizers(self):
+        # Prepare optimizer
+        param_optimizer = list(self.model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(
+                nd in n for nd in no_decay)], 'weight_decay': 0.01},
+            {'params': [p for n, p in param_optimizer if any(
+                nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters,
+                          lr=self.args.lr, correct_bias=False)
+        return optimizer
+
+    @staticmethod
+    def add_model_specific_args(parser):
+        parser.add_argument("--batch_size", type=int, default=2, help="Batch size")
+        parser.add_argument('--train_file', type=str, default='/ /train.csv', help='train file')
+        parser.add_argument('--valid_file', type=str, default='/ /valid.csv', help='valid file')
+        parser.add_argument('--test_file', type=str, default='/ /test.csv', help='test file')
+        
+        parser.add_argument("--model_path", type=str, default='longformer_kobart', help="Path to the checkpoint directory or model name")
+        parser.add_argument("--tokenizer_path", type=str, default='longformer_kobart')
+        
+        parser.add_argument("--gpus", type=int, default=1, help="Number of gpus. 0 for CPU")
+        parser.add_argument("--num_workers", type=int, default=5, help="Number of data loader workers")
+        parser.add_argument("--seed", type=int, default=1234, help="Seed")
+        parser.add_argument("--max_epochs", type=int, default=2, help="Number of epochs")
+        parser.add_argument("--max_output_len", type=int, default=1024, help="maximum num of output length. Used for training and testing")
+        parser.add_argument("--max_input_len", type=int, default=4096, help="maximum num of input length. Used for training and testing")
+        
+        parser.add_argument('--lr',type=float,default=1e-7,help='The initial learning rate')
+
+        parser.add_argument("--test", action='store_true', help="Test only, no training")
+        return parser
+
+
+def main(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.seed)
+
+    dm = LongformerSummaryModule(args.train_file, args.valid_file, args.test_file, args.tokenizer_path,
+                        batch_size=args.batch_size,
+                        max_input_len=args.max_input_len,
+                        max_output_len=args.max_output_len,
+                        num_workers=args.num_workers)
+
+    model = LongformerKobart(args)
+
+    # wandb logger 추가하기
+    wandb_logger = WandbLogger(project='longformer',name=f'Longformer_testing')
+    checkpoint_callback = ModelCheckpoint(dirpath='checkpoints', filename='{epoch:02d}-{val_loss:.3f}', save_top_k=2, verbose=True, monitor='val_loss', mode='min')
+    print(args)
+
+    trainer = pl.Trainer(gpus=args.gpus, 
+                        distributed_backend='ddp' if torch.cuda.is_available() else None,
+                        accumulate_grad_batches = 1 # if 4, 4 batch > 16 batch
+                        max_epochs=args.max_epochs,
+                        val_check_interval= 0.25, # check check validation set 4 times during a training epoch
+                        check_val_every_n_epoch=1, 
+                        logger=wandb_logger,
+                        checkpoint_callback=checkpoint_callback,
+                        gradient_clip_val=0.0 # No gradient clipping,
+                        log_every_n_steps=50 # logging frequency in training step,
+                        max_epochs = args.max_epochs)
+    if not args.test:
+        trainer.fit(model,dm)
+    trainer.test(model)
+
+if __name__ == "__main__":
+    main_arg_parser = argparse.ArgumentParser(description="longformer_kobart")
+    parser = LongformerKobart.add_model_specific_args(main_arg_parser)
+    args = parser.parse_args()
+    main(args)
