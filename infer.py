9ddd30a13e8b8aad20130cfd8038975dae19360c
shkim960520 <shkim960520@gmail.com>
2021-12-28 15:59:28 +0000

    modify path & device error

--- infer.py
+++ infer.py
@@ -6,8 +6,8 @@ from transformers import PreTrainedTokenizerFast
 
 def summarize_infer(
     text,
-    model_ckpt="./longformer_kobart_trained_ckpt",
-    tokenizer_ckpt="./longformer_kobart_initial_ckpt",
+    model_ckpt="model/longformer_kobart_trained_ckpt",
+    tokenizer_ckpt="model/longformer_kobart_initial_ckpt",
     max_length=1024,
     num_beams=5):
     
@@ -23,7 +23,7 @@ def summarize_infer(
     text += "</s>"  # end token 추가
     input_ids = tokenizer.encode(text)
     input_ids += [padding_idx] * (source_max_len-len(input_ids))
-    input_ids = torch.tensor(input_ids)
+    input_ids = torch.tensor(input_ids, device=device)
     input_ids = input_ids.unsqueeze(0) # input 형태에 따라 맞춰주어야 함
 
     output = model.generate(input_ids, eos_token_id=1, max_length=max_length, num_beams=num_beams)
@@ -34,6 +34,6 @@ def summarize_infer(
 
 if __name__ == "__main__":
     text = "안녕하세요"
-    output = summarize_infer(text, model_ckpt='./longformer_kobart_initial_ckpt')
+    output = summarize_infer(text, model_ckpt='model/longformer_kobart_initial_ckpt')
     print(output)
 
