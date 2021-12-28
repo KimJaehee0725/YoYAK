cacb518aef826e96e1a69a6e6b8c0f87cff4fae3
Lainshower <kaoara@naver.com>
2021-12-29 08:42:01 +0900

    change structure

--- get_model_binary.py
+++ get_model_binary.py
@@ -5,13 +5,13 @@ pytorch-ligtening binary에서 pytorch binary file로 변환하는 코드
 
 import argparse
 from train import LongformerKobart
-from make_longformer import *
+from model.make_longformer import *
 
 import yaml
 parser = argparse.ArgumentParser()
 parser.add_argument("--hparams", default=None, type=str) # logs/tb_logs/default/version_0/hparams.yaml
 parser.add_argument("--model_binary", default=None, type=str) # logs/model_chp/*.ckpt
-parser.add_argument("--output_dir", default='longformer_kobart_trained_ckpt', type=str) # pytorch binary가 저장되는 경로
+parser.add_argument("--output_dir", default='.model/longformer_kobart_trained_ckpt', type=str) # pytorch binary가 저장되는 경로
 args = parser.parse_args()
 
 with open(args.hparams) as f:
