456ce3326eca8cdfb396a7af007d961f78ea95d0
김재희(Kim Jaehee) <stapers@naver.com>
2022-01-07 18:12:23 +0900

    fine tuning 코드 완성!
    validation 및 test 관련 점검하면 끝날듯

--- finetuning/model_data_loader.py
+++ finetuning/model_data_loader.py
@@ -1,10 +1,13 @@
 import torch
+import os, sys
 
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from model.make_longformer import *
 from transformers import PreTrainedTokenizerFast
 from torch.utils.data import Dataset
 
 import pandas as pd
+import os
 
 def load_YoYak(
     model_ckpt = "model/longformer_kobart_trained_ckpt",
@@ -24,7 +27,7 @@ def load_YoYak(
 
 class FineTuningDataset(Dataset):
     def __init__(self, tokenizer, data_path, pad_idx = None, encoder_max_len = 4095, decoder_max_len = 1022) : 
-        super().__init()
+        super().__init__()
         self.tokenizer = tokenizer
         self.encoder_max_len = encoder_max_len
         self.decoder_max_len = decoder_max_len
@@ -43,21 +46,26 @@ class FineTuningDataset(Dataset):
         
         label_ids = self.tokenizer.encode(instance["summary_text"])[:self.decoder_max_len]
         label_ids.append(self.tokenizer.eos_token_id)
+        label_str = instance["summary_text"]
         
         decoder_input_ids = [self.tokenizer.eos_token_id]
-        decoder_input_ids.append(label_ids)
+        decoder_input_ids.extend(label_ids)
     
         return {"input_ids" : input_ids,
         "decoder_input_ids" : decoder_input_ids,
-        "label_ids" : label_ids}
+        "label_ids" : label_ids,
+        "label_str" : label_str}
+    
+    def __len__(self):
+        return self.data.shape[0]
 
 def collate_fn(batch) :
-    batch_size = batch.size()[0]
+    batch_size = len(batch)
     pad_id = 3
     non_attention_value = 0
     not_cal_for_softmax = -100
-    source_max_len  = 4096
-    target_max_len = 1024
+    source_max_len  = 1024 #4096
+    target_max_len = 512 #1024
     
     encoder_tensor = torch.full(size = (batch_size, source_max_len), fill_value = pad_id, requires_grad = False)
     encoder_attention_tensor = torch.full(size = (batch_size, source_max_len), fill_value = non_attention_value, requires_grad = False)
@@ -67,25 +75,31 @@ def collate_fn(batch) :
 
     label_tensor = torch.full(size = (batch_size, target_max_len), fill_value = not_cal_for_softmax, requires_grad = False)
     
+    label_strings = []
     for num, data in enumerate(batch) :
         encoder_ids = data["input_ids"]
+        encoder_ids = torch.tensor(encoder_ids)
         encoder_len = encoder_ids.size()[0]
         encoder_tensor[num, :encoder_len] = encoder_ids
         encoder_attention_tensor[num, :encoder_len] = 1
 
         decoder_ids = data["decoder_input_ids"]
+        decoder_ids = torch.tensor(decoder_ids)
         decoder_len = decoder_ids.size()[0]
         decoder_tensor[num, :decoder_len] = decoder_ids
         decoder_attention_tensor[num, :decoder_len] = 1
 
         label_ids = data["label_ids"]
+        label_ids = torch.tensor(label_ids)
         label_tensor[num, :decoder_len - 1] = label_ids
 
         encoder_dict = {"input_ids" : encoder_tensor, "attention_mask" : encoder_attention_tensor}
         decoder_dict = {"input_ids" : decoder_tensor, "attention_mask" : decoder_attention_tensor}
         label_dict = {"input_ids" : label_tensor}
 
-    return encoder_dict, decoder_dict, label_dict
+        label_strings.append(data["label_str"])
+
+    return encoder_dict, decoder_dict, label_dict, label_strings
     
 
 
