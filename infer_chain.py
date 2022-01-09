969bd2de8befce914abcf1cef381068e3c649d32
김재희(Kim Jaehee) <43404665+KimJaehee0725@users.noreply.github.com>
2022-01-09 18:12:24 +0900

    Add files via upload

--- infer_chain.py
+++ infer_chain.py
@@ -1,8 +1,40 @@
+import argparse
+
 from infer import summarize_infer
+from infer_models.load_kobigbird import mask_restore_infer_kobigbird, mask_restore_infer_kcbert
+from infer_models.load_koelectra import mask_overlap_concat, mask_overlap_average
 
-from infer_models.load_kobigbird import mask_restore_infer
+def main(args):
 
-input_str = input()
-summarized_str = summarize_infer(input_str)
-masked_str = mask_generate_infer(summarize_str)
-restored_str = mask_restore_infer(masked_str)
+    if args.masking_model == "overlap_concat":
+        from infer_models.load_koelectra import mask_overlap_concat
+        mask_generate_infer = mask_overlap_concat
+
+    elif args.masking_model == "overlap_average":
+        from infer_models.load_koelectra import mask_overlap_average
+        mask_generate_infer = mask_overlap_average
+
+    elif args.masking_model == "non-overlap":
+        from infer_models.load_koelectra import mask_per_510
+        mask_generate_infer = mask_per_510
+
+    if args.restore_model == "kobigbird":
+        mask_restore_infer = mask_restore_infer_kobigbird
+        
+    elif args.restore_model == "kcbert":
+        mask_restore_infer = mask_restore_infer_kcbert
+
+
+    input_str = input()
+    summarized_str = summarize_infer(input_str)
+    masked_str = mask_generate_infer(summarized_str, args.masking_threshold)
+    restored_str = mask_restore_infer(masked_str)
+    print(restored_str)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Let's summarize")
+    parser.add_argument("--masking_model", default = "overlap_concat", type = str, choices = ["overlap_concat", "overlap_average", "non-overlap"], help = "the model used for mask generation")
+    parser.add_argument("--masking_threshold", default = 0.2, type = float, help = "the threshold for logits of masking model")
+    parser.add_argument("--restore_model", default = "kobigbird", type = str, choices = ["kobigbird", "kcbert"], help = "the model used for restore masking")
+    args = parser.parse_args()
+    main(args)
