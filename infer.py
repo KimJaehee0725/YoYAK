54ba71f215b1cadf80b0140c3a667af989e13b27
김재희(Kim Jaehee) <stapers@naver.com>
2022-01-12 23:21:11 +0900

    batch 형태로 YoYak infer 할 수 있도록 수정

--- infer.py
+++ infer.py
@@ -1,24 +1,58 @@
 import torch
 
 from model.make_longformer import *
-from transformers import PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizerFast, BartForConditionalGeneration
+def load_model(model_ckpt, tokenizer_ckpt):
+    if "longformer" in model_ckpt:
+        model = LongformerBartForConditionalGeneration.from_pretrained(model_ckpt)
+        tokenizer = PreTrainedTokenizerFast.from_pretrained(tokenizer_ckpt)
+    else :
+        model = BartForConditionalGeneration.from_pretrained(model_ckpt)
+        tokenizer = PreTrainedTokenizerFast.from_pretrained(tokenizer_ckpt)
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    model.eval()
+
+    return model, tokenizer, device
+
+def summarize_batch_infer(
+    text_list,
+    model, 
+    tokenizer,
+    device,
+    target_max_length=1024,
+    source_max_len = 4096,
+    num_beams=5) :
+    padding_idx = 3
+
+    text_list = [text + "</s>" for text in text_list]  # end token 추가
+    input_ids = tokenizer(text_list, return_tensors = "pt", truncation = True, padding = True, max_length = 555).input_ids
+
+    if input_ids.size()[1] < target_max_length:
+        pad_tensor = torch.full(size = (input_ids.size()[0], source_max_len - input_ids.size()[1]), fill_value = padding_idx)
+        input_ids = torch.cat((input_ids, pad_tensor), dim = 1)
+    
+    else:
+        input_ids = input_ids[:, :target_max_length] # input 형태에 따라 맞춰주어야 함
+
+    input_ids = input_ids.to(device)
+    output = model.generate(input_ids, eos_token_id=1, max_length=target_max_length, num_beams=num_beams)
+    output = tokenizer.batch_decode(output, skip_special_tokens=True)
+    output = [text + "\n" for text in output]
+    return output   
 
 
 def summarize_infer(
     text,
-    model_ckpt="model/longformer_kobart_trained_ckpt",
-    tokenizer_ckpt="model/longformer_kobart_initial_ckpt",
+    model, 
+    tokenizer,
+    device,
     max_length=1024,
     num_beams=5):
     
     source_max_len = 4096
     padding_idx = 3
-
-    model = LongformerBartForConditionalGeneration.from_pretrained(model_ckpt)
-    tokenizer = PreTrainedTokenizerFast.from_pretrained(tokenizer_ckpt)
-
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    model.to(device)
 
     text += "</s>"  # end token 추가
     input_ids = tokenizer.encode(text)
@@ -29,11 +63,11 @@ def summarize_infer(
     output = model.generate(input_ids, eos_token_id=1, max_length=max_length, num_beams=num_beams)
     output = tokenizer.decode(output[0], skip_special_token=True)
 
-
     return output
 
 if __name__ == "__main__":
     text = "안녕하세요"
-    output = summarize_infer(text, model_ckpt='model/longformer_kobart_initial_ckpt')
+    model, tokenizer, device = load_model(model_ckpt='model/longformer_kobart_initial_ckpt', tokenizer_ckpt = "model/longformer_kobart_initial_ckpt")
+    output = summarize_infer(text, model = model, tokenizer = tokenizer, device = device)
     print(output)
 
--- infer_chain.py
+++ infer_chain.py
@@ -1,10 +1,10 @@
 import argparse
 
-# from infer import summarize_infer
+from infer import summarize_infer, load_model
 from infer_models.load_kobigbird import mask_to_replace
 from infer_models.load_koelectra import mask_overlap_concat, mask_overlap_average
 
-def infer_chain(input_str, masking_model = "overlap_concat", masking_threshold = 0.2):
+def infer_chain(summarize_model_ckpt, tokenizer_ckpt, input_str, masking_model = "overlap_concat", masking_threshold = 0.2):
 
     if masking_model == "overlap_concat":
         from infer_models.load_koelectra import mask_overlap_concat
@@ -20,7 +20,8 @@ def infer_chain(input_str, masking_model = "overlap_concat", masking_threshold =
 
     mask_restore_infer = mask_to_replace
 
-    # summarized_str = summarize_infer(input_str)
+    # model, tokenizer, device = load_model(model_ckpt = summarize_model_ckpt, tokenizer_ckpt = tokenizer_ckpt)
+    # summarized_str = summarize_infer(input_str, model, tokenizer, device)
     masked_str = mask_generate_infer(input_str, masking_threshold)
     restored_str = mask_restore_infer(masked_str)
     return restored_str 
@@ -34,5 +35,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Let's summarize")
     parser.add_argument("--masking_model", default = "overlap_concat", type = str, choices = ["overlap_concat", "overlap_average", "non-overlap"], help = "the model used for mask generation")
     parser.add_argument("--masking_threshold", default = 0.2, type = float, help = "the threshold for logits of masking model")
+    parser.add_argument("--model_ckpt", default = "model/longformer_kobart_initial_ckpt", type = str, help = "directory model saved")
+    parser.add_argument("--tokenizer_ckpt", default = "longformer_kobart_initial_ckpt", type = str, help = "directory tokenizer saved")
     args = parser.parse_args()
     main(args)
